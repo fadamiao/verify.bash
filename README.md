5b5bf31c1bbb2c17ce675ba3037647e5459b0321
Fernando A. Damião <me@fadamiao.com>
2013-09-02 11:05:23 -0300

    Choose the BSD 3-Clause License for verify.bash Change README.md to standard Markdown Change verify.bash code style Change .gitignore to UNIX Line Ending

--- README.md
+++ README.md
@@ -1,42 +1,37 @@
-verify.bash
-===========
+# verify.bash
 
 Just a way to help verify the compatibility of the system to run scripts or programs.
 
-## Specs
+
+## Infos
 * Made in OS : Mac OS X 10.6 (a.k.a Snow Leopard)
-* Tested in OS : Mac OS X 10.6, Mac OS X 10.7, Ubuntu 12.04 LTS
+* Tested in OS : Mac OS X 10.6, Mac OS X 10.7, Ubuntu Server 10.04 LTS, Ubuntu 12.04 LTS
 * Language Used : Shell Script (Bash)
 * Dependencies : type
+* License : BSD 3-Clause License
 
 
 ## How To
 ### Configuration
-Open verify.bash and change in the line 8 to bash version you want.
+Open verify.bash and change in the line `8` to bash version you want.
 
-```
-BASH_VER='3'
-```
+    BASH_VER='3'
 
 After this say the programs you want check.
 
-```
-PROGRAMS[0]='ps'
-PROGRAMS[1]='grep'
-PROGRAMS[2]='awk'
-PROGRAMS[3]='kill'
-```
+    PROGRAMS[0]='ps'
+    PROGRAMS[1]='grep'
+    PROGRAMS[2]='awk'
+    PROGRAMS[3]='kill'
 
 After this settings, you only need run verify.bash and be happy.
 
 
 ### Example of Execution
-```
-$ ./verify.bash 
-'ps'.... OK
-'grep'.... OK
-'awk'.... OK
-'kill'.... OK
-Bash version..... OK
-Passed!
-```
+    $ ./verify.bash 
+    'ps'.... OK
+    'grep'.... OK
+    'awk'.... OK
+    'kill'.... OK
+    Bash version..... OK
+    Passed!
--- verify.bash
+++ verify.bash
@@ -1,10 +1,16 @@
 #!/bin/bash
+# verify.bash - Just a way to help verify the compatibility of the system to run scripts or programs.
+# Author: Fernando A. Damião <fernando.damiao@inpe.br>
+# Created At: 2012-10-24 18:29
+# Last Update: 2013-09-02 10:59
+# License: BSD 3-Clause License
 
 # Initialization
 CONFIRMED='0'
 ELEMENTS='0'
 SYS=$(uname -s)
 # ---
+
 
 BASH_VER='3'
 
@@ -13,30 +19,33 @@ PROGRAMS[1]='grep'
 PROGRAMS[2]='awk'
 PROGRAMS[3]='kill'
 
-if [ "$SYS" == 'Darwin' -o "$SYS" == 'Linux' ]; then
-	for PROGRAM in ${PROGRAMS[@]}; do
-		ELEMENTS=$((ELEMENTS+1))
-		if ( type -P $PROGRAM &> /dev/null ); then
-			CONFIRMED=$((CONFIRMED+1))
-			echo "'$PROGRAM'.... OK"
-		else
-			echo "I need '$PROGRAM' to run"
-		fi
-	done
 
-	ELEMENTS=$((ELEMENTS+1))
-	if [ "$BASH_VERSINFO" -ge "$BASH_VER" ]; then
-		CONFIRMED=$((CONFIRMED+1))
-		echo "Bash version..... OK"
-	else
-		echo "Bash...   Failed"
-	fi
+if [ "${SYS}" == 'Darwin' -o "${SYS}" == 'Linux' ]; then
+    for PROGRAM in ${PROGRAMS[@]}; do
+        ELEMENTS=$((ELEMENTS + 1))
+        if ( type -P $PROGRAM &> /dev/null ); then
+            CONFIRMED=$((CONFIRMED + 1))
+            echo "'$PROGRAM'.... OK"
+        else
+            echo "I need '$PROGRAM' to run"
+        fi
+    done
 
-	if [ "$CONFIRMED" -eq "$ELEMENTS" ]; then
-		echo "Passed! You can run killer.bash"
-	else
-		echo "Install deps and run me after this..."
-	fi
+    ELEMENTS=$((ELEMENTS + 1))
+    if [ "${BASH_VERSINFO}" -ge "${BASH_VER}" ]; then
+        CONFIRMED=$((CONFIRMED + 1))
+        echo "Bash version..... OK"
+    else
+        echo "Bash...   Failed"
+    fi
+
+    if [ "${CONFIRMED}" -eq "${ELEMENTS}" ]; then
+        echo "Passed! You are able to run!"
+    else
+        echo "Install deps and run me after this..."
+        exit 1
+    fi
 else
-	echo "Your system is not supported"
+    echo "Your system is not supported"
+    exit 1
 fi
